3c361548525d9b85b4a07de749db9dd33ec31c3a
Fawwaz Anugrah W D <69834655+Wiradhika6051@users.noreply.github.com>
2022-01-19 22:19:11 +0700

    Menambahkan bg dan teks awal hero

---
+++ frontend/components/Hero.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+//import {Button} from './shared/Button'
+
+const Hero: NextPage = () => {
+	return (
+		<main className="p-4" id='hero'>
+			<header>
+			 <h1 className='big-title bold text-neutral-10'>Ramadhan dan Idul Adha bersama P3RI</h1>
+      		 <p className='headline text-neutral-60'>Maksimalkan amal ibadah anda dengan berbagai kegiatan dan materi yang bermanfaat bersama P3RI Salman ITB.</p>
+			</header>
+			//<Button />
+		</main>
+	)
+}
+
+export default Hero
