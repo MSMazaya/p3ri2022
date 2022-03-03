c11631bd56c08a7e92d74424bd904df631cb233c
MSMazaya <msulthanmazaya@gmail.com>
2022-03-03 12:56:02 +0700

    feature(frontend): add merch page baseline

---
+++ frontend/components/merch/Headline.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Headline = () => {
+	return (
+		<div className="m-5 grid grid-cols-2">
+			<div className="flex flex-col gap-4">
+				<div className="big-title bold text-neutral-1000">
+					Official Merch Store P3RI
+				</div>
+				<div className="headline medium text-neutral-900">
+					Maksimalkan amal ibadah anda dengan berbagai kegiatan dan materi yang bermanfaat bersama P3RI Salman ITB.
+				</div>
+				<div className="btn-primary w-fit">
+					Marketplace P3RI
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Headline
--- frontend/pages/merch.tsx
+++ frontend/pages/merch.tsx
@@ -1,8 +1,15 @@
+import Headline from 'components/merch/Headline'
+import Products from 'components/merch/Products'
+import Quote from 'components/merch/Quote'
 import React from 'react'
 
 const Merch = () => {
 	return (
-		<div>Merch</div>
+		<React.Fragment>
+			<Headline />
+			<Products />
+			<Quote />
+		</React.Fragment>
 	)
 }
 
