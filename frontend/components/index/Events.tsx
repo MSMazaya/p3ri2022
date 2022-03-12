e6900f23755a4313812ef548b05f3f3ac51a87d3
MSMazaya <msulthanmazaya@gmail.com>
2022-03-12 21:20:57 +0700

    feature(frontend): add carousel functionality

--- frontend/components/index/Events.tsx
+++ frontend/components/index/Events.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import React, { useState } from 'react'
 import { BiArrowBack } from "react-icons/bi";
 import { Event } from 'types/jadwal';
+import { useInterval } from 'ahooks';
 
 type Props = {
 	events: Event[];
@@ -11,49 +12,84 @@ type Props = {
 
 const Events = (props: Props) => {
 	const [selectedIndex, setSelectedIndex] = useState(0);
+	const scrollRef = React.useRef(null)
+	const [inUserClickedInterval, setInUserClickedInterval] = useState(false);
+
+	useInterval(() => {
+		if (!inUserClickedInterval) {
+			if (selectedIndex < props.events.length - 1) {
+				select(selectedIndex + 1);
+			} else {
+				select(0);
+			}
+		}
+	}, 5000)
 
 	const select = (index: number) => {
+		const { left } = document.getElementById(`${index}-scrollTo`).getBoundingClientRect();
+		scrollRef.current.scrollTo({
+			top: 0,
+			left: index ? left : 0,
+			behavior: 'smooth',
+		})
 		setSelectedIndex(index);
 	}
 
 	return (
-		<div className="p-10 grid grid-cols-2 relative my-10">
-			<div className="flex flex-col gap-10 m-10">
-				<div className="mid-title bold text-neutral-1000">
-					Banyak kegiatan yang kami buat, spesial untuk Anda
-				</div>
-				<div className="text-neutral-800 title-3 font-medium">
-					Event-event webinar, kajian, buka bersama dan banyak lagi lainnya kami buat untuk pengalaman di bulan Ramadhan dan Idul Adha yang terbaik untuk kita semua. InsyaAllah
-				</div>
-				<Link
-					href="/jadwal"
-					passHref
-				>
-					<div className="text-primary-main bold title-3 flex items-center gap-2 cursor-pointer group py-5">
-						Jadwal Lengkap
-						<BiArrowBack className="transform rotate-180 group-hover:animate-bounce-x" />
+		<>
+			<div className="p-10 grid grid-cols-2 relative my-10">
+				<div className="flex flex-col gap-10 m-10">
+					<div className="mid-title bold text-neutral-1000">
+						Banyak kegiatan yang kami buat, spesial untuk Anda
 					</div>
-				</Link>
-			</div>
-			<div className="w-fit overflow-x-hidden flex gap-3 pb-4">
-				{
-					props.events.map((event) => (<>
-						<EventCard event={event} />
-					</>))
-				}
-				<div className="absolute right-[15%] bottom-[4%] flex gap-3 select-none">
-					{
-						props.events.map((_, index) =>
-							<div
-								className={`bg-neutral-600 ${index === selectedIndex ? "" : "opacity-50"} rounded-xl p-2 shadow-xl cursor-pointer`}
-								key={index}
-								onClick={() => select(index)}
-							></div>
-						)
-					}
+					<div className="text-neutral-800 title-3 font-medium">
+						Event-event webinar, kajian, buka bersama dan banyak lagi lainnya kami buat untuk pengalaman di bulan Ramadhan dan Idul Adha yang terbaik untuk kita semua. InsyaAllah
+					</div>
+					<Link
+						href="/jadwal"
+						passHref
+					>
+						<div className="text-primary-main bold title-3 flex items-center gap-2 cursor-pointer group py-5">
+							Jadwal Lengkap
+							<BiArrowBack className="transform rotate-180 group-hover:animate-bounce-x" />
+						</div>
+					</Link>
+				</div>
+				<div className="overflow-x-scroll no-scrollbar" ref={scrollRef}>
+					<div className="w-fit overflow-x-hidden flex gap-3 pb-4">
+						{
+							props.events.map((event, index) => (<>
+								<EventCard event={event} />
+								<div id={`${index}-scrollTo`} />
+							</>))
+						}
+						<div className="absolute right-[15%] bottom-[4%] flex gap-3 select-none">
+							{
+								props.events.map((_, index) =>
+									<div
+										className={`bg-neutral-600 ${index === selectedIndex ? "" : "opacity-50"} rounded-xl p-2 shadow-xl cursor-pointer`}
+										key={index}
+										onClick={() => {
+											setInUserClickedInterval(true);
+											select(index);
+											setTimeout(() => {
+												setInUserClickedInterval(false);
+											}, 5000)
+										}}
+									></div>
+								)
+							}
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
+			<style jsx>{`
+				.no-scrollbar::-webkit-scrollbar {
+					display: none;
+				}
+			`}
+			</style>
+		</>
 	)
 }
 
