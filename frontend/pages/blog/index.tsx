c99ff51eb1bf383ab820593301ca3ef59b0f3a76
MSMazaya <msulthanmazaya@gmail.com>
2022-03-16 06:00:02 +0700

    refactor(frontend): change blog pagination style

--- frontend/pages/blog/index.tsx
+++ frontend/pages/blog/index.tsx
@@ -1,40 +1,33 @@
-import { useInViewport } from 'ahooks';
 import { BlogCard } from 'components/blog/BlogCard'
-import React, { useEffect, useRef } from 'react'
+import React, { useState } from 'react'
 import useSWRInfinite from 'swr/infinite'
 import { Article } from 'types/article';
 import fetcher from 'utils/fetcher';
 
-const TAKE = 8
-
-const getKey = (pageIndex: number, previousPageData) => {
-	if (previousPageData && !previousPageData.length) return null // reached the end
-	return `/api/blog?skip=${pageIndex * TAKE}&take=${TAKE}`                    // SWR key
-}
 
 const Index = () => {
-	const fetchRef = useRef();
-	const [inViewport] = useInViewport(fetchRef);
+	const [showButton, setShowButton] = useState(true);
+	const TAKE = 8
 
-	const { data, size, setSize } = useSWRInfinite<Article[]>(getKey, fetcher)
+	const { data, size, setSize } = useSWRInfinite<Article[]>(
+		(pageIndex, previousPageData) => {
+			if (previousPageData && previousPageData.length < TAKE) {
+				setShowButton(false);
+				return null;
+			}
+			return `/api/blog?skip=${pageIndex * TAKE}&take=${TAKE}`;
+		},
+		fetcher
+	)
 
-
-	useEffect(() => {
-		if (inViewport) {
-			setSize(size + 1);
-		}
-	}, [inViewport])
-
-	if (!data) {
-		return <h1>Loading...</h1>
-	}
-
+	const takeMoreData = () => setSize(size + 1)
 
 	return (
 		<div className="p-10 flex flex-col gap-4 min-h-screen mt-20 relative">
 			<div className="mid-title bold text-neutral-1000 text-center">Blog</div>
 			<div className="grid grid-cols-4 gap-10">
 				{
+					data &&
 					data.map(articles =>
 						articles.map((article, index) =>
 							<BlogCard article={article} key={index} />
@@ -42,7 +35,12 @@ const Index = () => {
 					)
 				}
 			</div>
-			<div className="absolute bottom-[2rem]" ref={fetchRef} />
+			{
+				data && (data.length >= TAKE) && showButton &&
+				<div className="btn-primary w-fit mx-auto py-4 my-4" onClick={takeMoreData}>
+					Show More
+				</div>
+			}
 		</div>
 	)
 }
